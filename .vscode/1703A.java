5d80efd0f483c56dbb4d9e5026432054f56884da
tanjimr <tanjimrubait@gmail.com>
2025-10-11 02:40:27 +0600

    Add solution for 1703A

---
+++ .vscode/1703A.java
@@ -0,0 +1,25 @@
+// althogh it can be done easily but i dont know the syntax which i will lean now
+
+public class 1703A {
+    public static void main(String[] args)
+    {
+
+    Scanner sc = new Scanner(System.in);
+
+    int t= sc.nextInt();
+    sc.nextLine();
+
+    for(int i=0; i<t; i++)
+    {
+        String s = sc.nextLine();
+
+        if((s.charAt(0)=='y' || s.charAt(0)=='Y') && 
+        (s.charAt(1)=='e' || s.charAt(1)=='E')&& 
+        (s.charAt(2)=='s' || s.charAt(2)=='S'))
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+    }
+}
+    
+}
