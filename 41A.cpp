22da7f425a09616afabc447d50d3d1fd106acf1e
tanjimr <tanjimrubait@gmail.com>
2025-10-18 00:00:48 +0600

    Add all solution

---
+++ 41A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    string in ;
+    cin >> in ;
+
+    int size = in.length();
+
+    reverse(in.begin(), in.end());
+
+    cout<< in ;
+
+    return 0;
+}
